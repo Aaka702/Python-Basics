4078a1f99446fdee1d429dc45090c9415a150328
Aaka702 <140441760+Aaka702@users.noreply.github.com>
2024-02-07 20:47:13 +0530

    Day1

---
+++ To find the largest of three integers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the first integer :"))
+b=int(input("Enter the second integer :"))
+c=int(input("Enter the third integer :"))
+if a>b:
+    print("largest is",a)
+elif b>c:
+    print("largest is",b)
+else:
+    print("largest is",c)
